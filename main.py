82a435b85e4f92a98ed871b38c78e4d66f2af2ad
pereznao <142930238+pereznao@users.noreply.github.com>
2023-09-08 17:51:42 +0000

    string formatting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# # Week  
+# # Week
 # # This week we will work on :
 # # Working With Strings
 
@@ -12,40 +12,32 @@
 # # 1.   Building a Basic Calculator
 # # 2.   Mad Libs Game
 
-
-
-
-
 # # Review
 # create variables for the following :
 # 1. age
-age=str(17)
+age = str(17)
 # 2. name
 name = "naomi"
 # 3. song
-song= "Room Temperature"
+song = "Room Temperature"
 # 4. food
 food = "pizza"
 # 5. number
 number = str(40)
 # #now include the variables you just made print in the following...
 
-# Once upon a time, there was a [age] old coder named [name]. 
-print("Once upon a time, there was a " + age + " old coder named " + name )
+# Once upon a time, there was a [age] old coder named [name].
+print("Once upon a time, there was a " + age + " old coder named " + name)
 # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
-print(name + " liked to hum the song " + song + " while coding. It was so annoying that their teammates would throw " + food + " until " + name + " would stop singing.")
+print(name + " liked to hum the song " + song +
+      " while coding. It was so annoying that their teammates would throw " +
+      food + " until " + name + " would stop singing.")
 
 # Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
-print("still, " + name + " was the best coder on the team and could write " + number + " lines of code every day. Maybe " + song + " was " + name + "'s secret power.")
+print("still, " + name + " was the best coder on the team and could write " +
+      number + " lines of code every day. Maybe " + song + " was " + name +
+      "'s secret power.")
 ##########################################################################################
-
-
-
-
-
-
-
-
 
 ##########################################################################################
 # The names you use when creating these labels need to follow a few rules:
@@ -53,7 +45,7 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 # 2. There can be no spaces in the name, use _ instead.
 # 3. Can't use any of these symbols :'",<>/?|\()!@#$%^&*~-+
 # 4. It's considered best practice (PEP8) that names are lowercase.
-# 5. Avoid using the characters 'l' (lowercase letter el), 'O' (uppercase letter oh), 
+# 5. Avoid using the characters 'l' (lowercase letter el), 'O' (uppercase letter oh),
 #    or 'I' (uppercase letter eye) as single character variable names.
 # 6. Avoid using words that have special meaning in Python like "list" and "str"
 
@@ -76,7 +68,6 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 # The percentage of marks obtained by a student
 # A variable to store the number of items in a shopping cart
 
-
 # Identify Valid and Invalid Names: Identify which of the following names are valid or invalid according to the rules:
 
 # first_name
@@ -89,12 +80,8 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 # percentage%
 # i
 
-
-
-
 # Declare two variables, called name and age.
 # Set the name variable value to "Tony Soprano" and the age value to 51.
-
 
 # Variables Practice #2
 # Create three variables:
@@ -103,13 +90,10 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 # full_name
 # Assign the value "Julia" to first_name, and for last_name, assign the value "Roberts". Finally, build the variable full_name by concatenating these two variables (remember to add a space in between)
 
-
 # Variables Practice #3
 # Declare the variable course, assign it the value "Python", and print the following sentence:
 # You are taking a course course
 # To do this, you must concatenate the first and last parts of the sentence with the variable. Remember to add spaces before and after concatenating the variable to the rest of the text.
-
-
 
 ################ # **Working with** **numbers** **bold text**#########
 
@@ -118,20 +102,49 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 # 2. Basic Arithmetic
 # 3. Differences between classic division and floor division
 
-# Python has various "types" of numbers (numeric literals). 
+# Python has various "types" of numbers (numeric literals).
 # 1. We'll mainly focus on integers and floating point numbers.
 # Integers are just whole numbers, positive or negative. For example: 2 and -2 are examples of integers.
 # 2. Floating point numbers in Python are notable because they have a decimal point in them, or use an exponential (e) to define the number. For example 2.0 and -2.1 are examples of floating point numbers. 4E2 (4 times 10 to the power of 2) is also an example of a floating point number in Python.
 
 # slides 10 -12
+##integers
+num1 = 55
+num2 = -55
+print(num1)
+print(num2)
+print(num1 + num2)
+print(type(num1))
+print(type(num2))
+#python knows a lot of number ty[es implicitly without you directly telling it(explicit)
+
+#floats -- decimal numbers
+num3 = 3.14
+print(type(num3))
+
+##explicit types
+age = "30"
+print(type(age))
+age = int(age)  ###telling python to convert to an int instead of str
+print(type(age))
+moms_age = int(input("how old is your mom? "))
+print(type(moms_age))
+siblings_age = input("how old are your siblings? ")
+print(type(siblings_age))
+
 # Integers Practice
 # Declare a numeric variable named int_num that contains a value of integer type of your choice.
 # Print the data type of that variable.
-
+int_num = 20
+print(int_num)
+print(type(int_num))
 
 # Floats Practice
 # Declare a numeric variable named decimal_num that contains a value of float type of your choice.
 # Print the data type of that variable.
+decimal_num = 12.5
+print(decimal_num)
+print(type(decimal_num))
 
 # ata Types Practice
 # What type is the result of the sum of 7.5 + 2.5? Write the code to verify it.
@@ -139,46 +152,63 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 # num1 = 7.5
 # num2 = 2.5
 # Next, print on the screen the data type that results from the sum of both numbers.
+num1 = 7.5
+num2 = 2.5
+num1 = float(num1)
+num2 = float(num2)
+print(num1 + num2)
 
 #################################Data Type conversions####################
 # slides 12 -19
 # Data Type Conversions Practice #1
 # Convert the value of num1 to an integer and print the resulting data type.
-
+num1 = int(num1)
+print(type(num1))
 #   Data Type Conversions Practice #2
 # Convert the value of num2 to a float and print the resulting data type.
-
+num2 = float(num2)
+print(type(num2))
 #   Data Type Conversions Practice #3
 # Add the values of num1 and num2.
 # Do not modify the value of variables already declared, but apply the necessary conversions within the print() function.
+print(num2 + num1)
 
 #################################formatting strings####################
 # slide 19 -22
-
 
 # Strings Formatting Practice #1
 # We need to print the associate name and number within the following sentence:
 # "Dear (associate_name), your associate number is: (associate_number)"
 # Remember that the precision of your answer (spaces, spelling and punctuation) is very important to arrive at the correct result.
 
-
 # associate_name = "Jesse Pinkman"
 # associate_number = 399058
 
 # Dear (associate_name), your associate number is: (associate_number)
+associate_name = "jesse pinkman"
+associate_number = 399058
+associate_number = str(associate_number)
+print(f"Dear {associate_name} , your associate number is:  {associate_number}")
 
 # Strings Formatting Practice #2
 # Tell the user the amount of points earned within the following phrase:
 # "You have earned (new_points) points! In total, you have accumulated (total_points) points"
 # Remember that the precision of your answer (spaces, spelling and punctuation) is very important to arrive at the correct result
-
+user = "araceli"
+new_points = 10000
+new_points = str(new_points)
+total_points = 10500
+total_points = str(total_points)
+print(
+    f"{user} , you have earned {new_points} points! In total, you have accumulated {total_points} points!"
+)
 
 # Strings Formatting Practice #3
 # Tell the user the amount of points earned within the following phrase:
 # "You have earned (new_points) points! In total, you have accumulated (total_points) points"
 # This time, the amount of points accumulated (total_points) will be equal to the previous_points plus the new_points.
 # Remember that the precision of your answer (spaces, spelling and punctuation) is very important to arrive at the correct result.
-
+#####
 # previous_points = 875
 # new_points = 350
 
@@ -194,7 +224,7 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 # # absolute value
 # # order of operations
 # #to do more you need to import special math libraries from python
-# #from math import *     
+# #from math import *
 # #this goes out and grabs some different math functions we can use
 # #floor method
 # #ceil method
@@ -253,21 +283,11 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 
 # Note: For the problems involving floor, ceil, and sqrt, students will need to use the `math` library functions in Python.
 
-
 # Print on the screen the floor division of the following two numbers: 874 divided by 27
-
-
 
 # Print on the screen the modulus of 456 divided by 33
 
-
-
 # Calculate and print the square root of 783
-
-
-
-
-
 
 ##########################################################################################
 # So what have we learned? We learned some of the basics of numbers in Python. We also learned how to do arithmetic and use Python as a basic calculator. We then wrapped it up with learning about Variable Assignment in Python.
@@ -283,21 +303,9 @@ print("still, " + name + " was the best coder on the team and could write " + nu
 # # find the remainder of the numbers
 # #round one number
 
-
-
-
-
-
-
-
 ######### on to the last slide for your challenge-- work on this with another person .
 
 # 1) create a new repl for the challenge
 # 2) name it challenge week 3
 # 3) invite the other person to your repl.
 # 4) you now have a mind partner in collaborating on solving this challenge
-
-
-
-
-
